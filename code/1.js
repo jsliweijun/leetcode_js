3e7bfb5b2d4b3d89cf28c5627f7acd2c06f55524
Oliver <jsliweijun@163.com>
2024-01-14 15:09:30 +0800

    add

--- code/1.js
+++ code/1.js
@@ -3,7 +3,25 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {};
+var twoSum = function (nums, target) {
+    const mapObj = {};
+    const resArr = [];
+    for (i = 0; i < nums.length; i++) {
+        const currentValue = nums[i];
+        const value = target - currentValue;
+        const valueIndex = mapObj[value];
+
+        // 索引可能是 0
+        if (valueIndex || valueIndex === 0) {
+            resArr.push(i);
+            resArr.push(valueIndex);
+            return resArr;
+        } else {
+            mapObj[currentValue] = i; // 当前值和它的索引
+        }
+    }
+    return resArr;
+};
 
 // case1
 const case1 = { nums: [2, 7, 11, 15], target: 9 };
