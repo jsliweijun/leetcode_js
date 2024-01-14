42c3176ee97dc35a20640d288b4f1366f15b4174
Oliver <jsliweijun@163.com>
2024-01-14 09:02:08 +0800

    add

---
+++ code/1.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {};
+
+// case1
+const case1 = { nums: [2, 7, 11, 15], target: 9 };
+console.log(twoSum(case1.nums, case1.target));
